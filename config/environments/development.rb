5dd5d17d2ae5341b4c90acd3e8455fff8b4c75ac
stozuka <stozuka@gmail.com>
2017-04-29 07:43:09 +0000

    Finish chapter 12

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'rails-tutorial-mhartl.c9users.io'
+  host = 'rails-tutorial-stozuka.c9users.io'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
