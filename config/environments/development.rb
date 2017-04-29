9a8cd0bd92539e0bd409e67e7b6d3531f8db918c
stozuka <stozuka@gmail.com>
2017-04-29 11:24:33 +0000

    Finish chapter 13

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'rails-tutorial-stozuka.c9users.io'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: host, port: 8080, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
